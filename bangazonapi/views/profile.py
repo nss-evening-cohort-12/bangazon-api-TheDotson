1f2f61175174ef583f2b6849dacedadfdceca530
Michael Dotson <thedotson@icloud.com>
2021-02-09 20:55:57 -0600

    requested changes

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -71,7 +71,7 @@ class Profile(ViewSet):
     def cart(self, request):
         """Shopping cart manipulation"""
 
-        current_user = Customer.objects.get(user__id=request.auth.user.id)
+        current_user = Customer.objects.get(user=request.auth.user)
 
         if request.method == "DELETE":
             """
