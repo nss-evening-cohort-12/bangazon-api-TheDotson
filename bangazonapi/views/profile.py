af67c7d9b5d76913e85c19c3b529bb208a76ffd0
Michael Dotson <thedotson@icloud.com>
2021-02-16 19:02:07 -0600

    added function to favorite a seller

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -226,7 +226,7 @@ class Profile(ViewSet):
 
         return Response({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    @action(methods=['get'], detail=False)
+    @action(methods=['get', 'post'], detail=False)
     def favoritesellers(self, request):
         """
         @api {GET} /profile/favoritesellers GET favorite sellers
@@ -277,9 +277,21 @@ class Profile(ViewSet):
         customer = Customer.objects.get(user=request.auth.user)
         favorites = Favorite.objects.filter(customer=customer)
 
+        if request.method == "POST":
+            favorite_seller = Favorite()
+            favorite_seller.seller = Customer.objects.get(pk=request.data["seller"])
+            favorite_seller.customer = customer
+            favorite_seller.save()
+
+            favorite_seller_json = FavoriteSerializer(favorite_seller, many=False, context={'request': request})
+
+            return Response(favorite_seller_json.data)
+
         serializer = FavoriteSerializer(
             favorites, many=True, context={'request': request})
         return Response(serializer.data)
+
+        return Response({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 class LineItemSerializer(serializers.HyperlinkedModelSerializer):
